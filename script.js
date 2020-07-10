7b383ab6fd58ed72acf8c3d80db33c1e4c361d51
OkieKenobi <travismartinez88@gmail.comgit config --global user.name Tmartinez0202>
2020-07-10 13:55:49 -0600

    fixed issues

--- script.js
+++ script.js
@@ -1,42 +1,3 @@
-<<<<<<< HEAD
-$(document).ready(function(){
-//Onclick to get value of input and store in variable
-
-$('#submit-button').on('click', function() {
-  event.preventDefault();
-  var userInput = $('#user-input').val();
-  console.log(userInput);
-  
-
-
-var searchTerm = "breweries";
-// var searchLocation = "80301";
-
-function searchYelp() {
-  $.ajax({
-    url: `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${searchTerm}&location=${userInput}`,
-    method: "GET",
-    headers: {
-      "Authorization": "Bearer AmvxFoRRCm1gPrumi5fAr7uEGkpOAeDoFKKAjJ_zhHo4uB4-RFb2GLyvQ-CBbVvdi7syTFO5y2WIgTYOqfWdaUp6duFzLRfCGh5-0o6Blh3AQlKgvy-jGqvLui8HX3Yx"
-    }
-  }).then(function (response) {
-    console.log(response);
-    $("#top").empty()
-    for (var i = 0; i < response.businesses.length; i++){
-       var row = $("<tr>")
-       var name = $("<td>").text(response.businesses[i].name)
-       var rating = $("<td>").text("Stars: " + response.businesses[i].rating)
-       var phone = $("<td>").text("Phone: " + response.businesses[i].phone)
-       $("#top").append(row)
-       row.append(name)
-       name.append(phone)
-       phone.append(rating)
-    }
-  });
-}
-searchYelp()
-
-=======
 $(document).ready(function() {
 	//Onclick to get value of input and store in variable
 
@@ -56,14 +17,18 @@ $(document).ready(function() {
 					Authorization:
 						'Bearer AmvxFoRRCm1gPrumi5fAr7uEGkpOAeDoFKKAjJ_zhHo4uB4-RFb2GLyvQ-CBbVvdi7syTFO5y2WIgTYOqfWdaUp6duFzLRfCGh5-0o6Blh3AQlKgvy-jGqvLui8HX3Yx'
 				}
-			}).then(function(response) {
+			}).then(function (response) {
 				console.log(response);
-				$('#top').empty();
-				for (var i = 0; i < response.businesses.length; i++) {
-					var name = $('<li>').text(response.businesses[i].name);
-					var rating = $('<li>').text(response.businesses[i].rating);
-					var phone = $('<li>').text(response.businesses[i].phone);
-					$('#top').append(name, phone, rating);
+				$("#top").empty()
+				for (var i = 0; i < response.businesses.length; i++){
+				   var row = $("<tr>")
+				   var name = $("<td>").text(response.businesses[i].name)
+				   var rating = $("<td>").text("Stars: " + response.businesses[i].rating)
+				   var phone = $("<td>").text("Phone: " + response.businesses[i].phone)
+				   $("#top").append(row)
+				   row.append(name)
+				   name.append(phone)
+				   phone.append(rating)
 					var lats = response.businesses[i].coordinates.latitude;
 					var longs = response.businesses[i].coordinates.longitude;
 					var latLong = { lat: lats, lng: longs };
@@ -92,5 +57,4 @@ $(document).ready(function() {
 		searchYelp();
 		initMap();
 	});
->>>>>>> master
 });
